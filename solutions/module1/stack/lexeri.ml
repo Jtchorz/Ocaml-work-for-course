10d564e6ebbfbf527b9f19a62f3f83106d612bd6
jantc <jantc@kth.se>
2025-11-09 13:14:06 +0100

    all works, not through rigorous testing, eror messages still print

--- solutions/module1/stack/lexeri.ml
+++ solutions/module1/stack/lexeri.ml
@@ -2,16 +2,16 @@ open Printf
 open Buffer
 let char_cnt = ref 0
 let line_cnt = ref 0
-let currentChar = ref 'a'
+let currentChar : char option ref = ref None
 let atEOF = ref false
 let nextChar () = 
   try (let c = input_char stdin in 
-  currentChar := c;
+  currentChar := Some c;
   incr char_cnt)
   with
   |End_of_file -> atEOF := true
 
-type token = 
+type token =
   | Num of int
   | Push
   | Semi 
@@ -28,8 +28,10 @@ let rec gets () =
   let b = Buffer.create 4 in
   let rec work b = 
     match !currentChar with 
-    | 'a' .. 'z' -> Buffer.add_char b !currentChar; nextChar(); work b
-    | _ -> b
+    | Some c -> (match c with
+      | 'a' .. 'z' -> Buffer.add_char b c; nextChar(); work b
+      | _ -> b)
+    | None -> printf "very unexpected error during lexing at line %d char end at %d \n" !line_cnt !char_cnt ; exit 1
   in work b
 
 let getcomm () =
@@ -42,17 +44,36 @@ let getcomm () =
   | "mul" -> Mul
   | "div" -> Div
   | "show" -> Show
-  | _ -> exit 1
+  | _ -> printf "not a valid command at line %d char end at %d \n" !line_cnt !char_cnt ; exit 1
+
+let rec getn () = 
+  let b = Buffer.create 10 in
+  let rec work b = 
+    match !currentChar with 
+    | Some c -> (match c with
+      | '0' .. '9' -> Buffer.add_char b c; nextChar(); work b
+      | _ -> b)
+    | None -> printf "very unexpected error during lexing at line %d char end at %d \n" !line_cnt !char_cnt ; exit 1
+  in work b
+
+let getnum () =
+  let s = Buffer.contents(getn () ) in
+  try Num(int_of_string s) with
+  | Failure _ -> printf "not a valid number at line %d ending at character %d \n" !line_cnt !char_cnt; exit 1
+
+
 let rec nextToken () =
-  nextChar();
-  if (!atEOF) then
-    Eof
-  else(
-  match !currentChar with
-  | '\n' -> incr line_cnt; char_cnt := 0; Endline
-  | ';' -> Semi
-  | ' ' | '\t' -> nextToken ()
-  | 'p' | 'a' | 's' | 'm' | 'd' -> getcomm ()
-  | '0' .. '9' as n -> Num (int_of_char n)
-  | _ -> (printf "Unrecognized char at line %d char %d \n" !line_cnt !char_cnt; exit 1)
+    if (!atEOF) then
+      Eof
+    else(
+    match !currentChar with
+    | Some c -> (match c with
+      | '\n' -> incr line_cnt; char_cnt := 0; currentChar := None; Endline
+      | ';' -> nextChar(); Semi
+      | ' ' | '\t' -> nextChar(); nextToken ()
+      | 'p' | 'a' | 's' | 'm' | 'd' -> getcomm ()
+      | '0' .. '9' -> getnum ()
+      | _ -> (printf "Unrecognized char at line %d char %d \n" !line_cnt !char_cnt; exit 1)
+      )
+    | None -> nextChar(); decr char_cnt; nextToken()
   )
--- solutions/module1/stack/stack.ml
+++ solutions/module1/stack/stack.ml
@@ -1,45 +1,126 @@
-(* To parse command-line arguments, checkout OCamls `Arg` module
-   (https://ocaml.org/manual/5.1/api/Arg.html). To control the exit code on
-   errors you can use `Arg.parse_argv` instead of `Arg.parse` *)
 open Printf
 open Lexeri
 
-type tree =
-  | Node of string * tree * tree
-  | Leaf of int
+type expr =
+   | PushE of int
+   | PopE
+   | AddE
+   | SubE
+   | MulE
+   | DivE
+   | ShowE 
+
+type exprs =
+   | Many of expr * exprs
+   | One of expr
+let prettytok t = 
+   match t with
+      | Num n -> printf ("Num %d \n") n
+      | Push -> printf ("Push\n")
+      | Semi -> printf ("Semi\n") 
+      | Pop -> printf ("Pop\n")
+      | Add -> printf ("Add\n") 
+      | Sub -> printf ("Sub\n") 
+      | Mul -> printf ("Mul\n") 
+      | Div -> printf ("Div\n")
+      | Show -> printf ("Show\n")
+      | Endline -> printf ("Endline\n")
+      | Eof -> printf ("Eof\n")
+
+let prettyexpr t = 
+   match t with
+      | PushE(n) -> printf ("Push %d \n") n
+      | PopE -> printf ("Pop\n")
+      | AddE -> printf ("Add\n") 
+      | SubE -> printf ("Sub\n") 
+      | MulE -> printf ("Mul\n") 
+      | DivE -> printf ("Div\n")
+      | ShowE -> printf ("Show\n")
+let parseExpr t = 
+   match t with 
+      | Push -> (let nt = nextToken () in match nt with
+         | Num num -> (let nt2 = nextToken () in match nt2 with
+            | Semi -> (nextToken (), PushE(num))
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+         | _ -> printf "not a number \n"; exit 1
+         )
+      | Pop -> (match nextToken () with
+            | Semi -> (nextToken (), PopE)
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+      | Add -> (match nextToken () with
+            | Semi -> (nextToken (), AddE)
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+      | Sub -> (match nextToken () with
+            | Semi -> (nextToken (), SubE)
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+      | Mul -> (match nextToken () with
+            | Semi -> (nextToken (), MulE)
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+      | Div -> (match nextToken () with
+            | Semi -> (nextToken (), DivE)
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+      | Show -> (match nextToken () with
+            | Semi -> (nextToken (), ShowE)
+            | _ -> printf "error missing semicolon\n"; exit 1
+            )
+      | _ -> printf "error, expected a command token \n"; exit 1
+
+let rec parseExprs t =
+   let (nt, exp) = parseExpr t in match nt with
+   | Eof | Endline -> (nt, One(exp)) (*only will check these as parseExpr can handle not getting a word token, no reason to double up err handling*)
+   | _ -> let (nt2, exps) = parseExprs nt in (nt2, Many(exp, exps))
+
+let rec prettyast e = 
+   match e with
+   | One(exp) -> prettyexpr exp
+   | Many(exp, exps) -> prettyexpr exp; prettyast exps
+
+let execute e stack= 
+   match e with
+   | PushE(n) -> n :: stack
+   | PopE -> (match stack with
+      | [] -> printf "error, nothing to pop \n"; exit 2
+      | _::nstack -> nstack
+      )
+   | AddE -> (match stack with
+      | a::b::nstack -> (a+b)::b::nstack
+      | [] | [_] -> printf "error, not enough elements to execute operation \n"; exit 2
+
+      )
+   | SubE -> (match stack with
+      | a::b::nstack -> (a-b)::b::nstack
+      | [] | [_] -> printf "error, not enough elements to execute operation \n"; exit 2
+      )
+   | MulE -> (match stack with
+      | a::b::nstack -> (a*b)::b::nstack
+      | [] | [_] -> printf "error, not enough elements to execute operation \n"; exit 2
+      )
+   | DivE -> (match stack with
+      | a::b::nstack -> (a/b)::b::nstack
+      | [] | [_] -> printf "error, not enough elements to execute operation \n"; exit 2
+      )
+   | ShowE -> (match stack with
+      | [] -> printf "stack is empty \n"; stack
+      | a::nstack -> printf "%d\n" a; a::nstack
+      )
 
 
-let prettys lst = 
-   let rec loop lst =
-      match lst with
-      | [] -> ""
-      | [x] -> (*string_of_int*) x
-      | x :: l -> (*string_of_int*) x ^ "\n" ^ loop l
-   in 
-      loop lst
+let rec executeall e stack =
+   match e with
+   | One(exp) -> ignore(execute exp stack); ()
+   | Many(exp, exps) -> executeall exps (execute exp stack)
 
-let prettyi lst = 
-   let rec loop lst =
-      match lst with
-      | [] -> ""
-      | [x] -> string_of_int x
-      | x :: l -> string_of_int x ^ "," ^ loop l
-   in 
-      loop lst
+let rec main () =
+   let next = nextToken() in 
+   let (t, e) = parseExprs next in match t with
+   | Eof -> executeall e []
+   | Endline -> executeall e []; flush stdout; main() (*printf "Ast constructed: \n"; prettyast e; exit 0*)
+   | _ -> printf "Unexpected parsing error"; exit 1
 
-let main =
-     let te = nextToken() in let rec work t =
-      match t with
-         | Num n -> printf ("Num %d \n") n; work (nextToken())
-         | Push -> printf ("push\n"); work (nextToken())
-         | Semi -> printf ("Semi\n"); work (nextToken())
-         | Pop -> printf ("Pop\n"); work (nextToken())
-         | Add -> printf ("Add\n"); work (nextToken())
-         | Sub -> printf ("Sub\n"); work (nextToken())
-         | Mul -> printf ("Mul\n"); work (nextToken())
-         | Div -> printf ("Div\n"); work (nextToken())
-         | Show -> printf ("Show\n"); work (nextToken())
-         | Endline -> printf ("Endline\n%!"); work (nextToken())
-         | Eof -> printf ("Eof\n%!");
-     in work te
-      
+let () = main ()
