d9006b04bcce71dd3dfc1b0a0d5e76dd1bd7d490
jantc <jantc@kth.se>
2025-11-24 18:44:57 +0100

    fixed err handling v3 #module2-cigrid-s

--- solutions/cigrid/instrSelection.ml
+++ solutions/cigrid/instrSelection.ml
@@ -15,7 +15,7 @@ let rec expr_to_asm env n acc reg = function
   | EVar(name, _) -> ((BinOp(Mov, reg, find_reg name env))::acc, n)
   | EInt(nu, _) -> ((BinOp(Mov, reg, Imm(nu)))::acc,n)
   | EChar(c,_) -> ((BinOp(Mov, reg, Imm(int_of_char c)))::acc,n)
-  | EString(s, _) -> printf "stringTODO"; exit 4
+  | EString(s, _) -> failwith "stringTODO"
   | EBinOp(BopAdd,e1,e2,_) -> 
     let (r1, r2) = (tmp_reg n, tmp_reg(n+1)) in
     let n1 = n+2 in 
@@ -28,13 +28,13 @@ let rec expr_to_asm env n acc reg = function
     let acc1 = (BinOp(Mov, reg, r1))::(BinOp(Sub, reg, r2))::acc in 
     let (acc2, n2) = expr_to_asm env n1 acc1 r2 e2 in 
     expr_to_asm env n2 acc2 r1 e1
-  | EBinOp(uop,e1,e2,_) -> printf "otherbinopTODO"; exit 4
+  | EBinOp(uop,e1,e2,_) -> failwith "otherbinopTODO"
   | EUnOp(UnOpMinus, exp,_) -> let acc1 = (UnOp(Neg, reg))::acc in
     expr_to_asm env n acc1 reg exp 
-  | EUnOp(uop, exp,_) -> printf "otherunopTODO"; exit 4
-  | ECall(name, exprList, _) -> printf "ecallTODO"; exit 4
-  | ENew(t,exp,_) -> printf "enewTODO"; exit 4
-  | EArrayAccess(name,elementNum,structfieldopt,_)-> printf "earrayaccessTODO"; exit 4
+  | EUnOp(uop, exp,_) -> failwith "otherunopTODO"
+  | ECall(name, exprList, _) -> failwith "ecallTODO"
+  | ENew(t,exp,_) -> failwith "enewTODO"
+  | EArrayAccess(name,elementNum,structfieldopt,_)-> failwith "earrayaccessTODO"
 
 let rec irstmt_list_to_asm env n acc = function
   | ISVarDecl(name, t, _)::restlist -> let nenv = (name,(n,t))::env in
@@ -42,7 +42,7 @@ let rec irstmt_list_to_asm env n acc = function
   | ISVarAssign(name, exp, _)::restlist ->  
     let (eacc, n2) = expr_to_asm env n [] (find_reg name env) exp in
     irstmt_list_to_asm env n2 (List.rev_append eacc acc) restlist
-  | ISExpr(e,_)::restlist -> printf "ISExprTODO"; exit 4
+  | ISExpr(e,_)::restlist -> failwith "ISExprTODO"
   | [] -> (env, n, List.rev acc)
 
 
@@ -53,8 +53,8 @@ let ir_blockend env n acc = function
       ((Ret), acc@eacc,n1)
     | None -> ((Ret), acc, n)
   )
-  | ISBranch(exp,n1,n2,_) -> printf "ISExprTODO"; exit 4
-  | ISJump(name,_) -> printf "ISExprTODO"; exit 4
+  | ISBranch(exp,n1,n2,_) -> failwith "ISExprTODO"
+  | ISJump(name,_) -> failwith "ISExprTODO"
 
 
 
@@ -64,7 +64,7 @@ let bop_spill bop acc op1 op2 =
       match op1 with 
       | TReg((n,b),_) -> (BinOp(bop, Mem(b,(4,QWord),None,0,(n*8)), op2))::acc
       | Reg(_) | Mem(_) -> (BinOp(bop, op1, op2))::acc
-      | Imm(_) | NoOp -> printf "impossible"; exit 999
+      | Imm(_) | NoOp -> failwith "impossible"; exit 999
       )
     | TReg((n,b),_) -> (
       match op1 with  
@@ -72,16 +72,16 @@ let bop_spill bop acc op1 op2 =
         (BinOp(bop, op1, Mem(b,(4,QWord),None,0,(n*8))))::acc
       | TReg((n1,b1),_) -> 
         (BinOp(bop, Mem(b1,(4,QWord),None,0,(n1*8)),Reg(10,QWord)))::(BinOp(Mov, Reg(10,QWord), Mem(b,(4,QWord),None,0,(n*8))))::acc
-      | Mem(_) -> printf "TODOMem"; exit 999
-      | Imm(_) | NoOp -> printf "impossible"; exit 999
+      | Mem(_) -> failwith "TODOMem"; exit 999
+      | Imm(_) | NoOp -> failwith "impossible"; exit 999
       )
-    | _ -> printf "impossible"; exit 999
+    | _ -> failwith "impossible"; exit 999
 
 let rec reg_alloc n acc = function
     | BinOp(bop, op1, op2)::restlist -> let acc2 = bop_spill bop acc op1 op2 in 
       reg_alloc n acc2 restlist
     | [] -> List.rev ((BinOp(Add, Reg(4,QWord),Imm(n*8)))::acc)
-    | _ -> printf "only doing bop for now"; exit 4
+    | _ -> failwith "only doing bop for now"
 
 
 
